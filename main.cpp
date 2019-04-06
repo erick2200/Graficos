a7de37afeee389f911df7e0ee227ff9d39b8117a
erick2200 <egutierreze@unsa.edu.pe>
2019-04-06 07:14:09 -0500

    avance 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow *,int,int);
 void process_input(GLFWwindow*);
@@ -9,21 +10,22 @@ void process_input(GLFWwindow*);
 const char *vertexShaderCode =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
-    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
+    "   ourColor = aColor;\n"
     "}\0"
 ;
 
 const char *fragmentShaderCode =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "in vec4 vertexColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-        "FragColor = vertexColor;\n"
+        "FragColor = vec4(ourColor,1.0f);\n"
     "}\n\0"
 ;
 
@@ -101,36 +103,32 @@ int main()
     //cordenadas del triangulo
     float VertexData[] =
     {
-        -0.5f , 0.5f ,0.0f,     //izquierda arriba 0
-        -0.5f ,-0.5f ,0.0f,     //izquierda abajo  1
-         0.5f ,-0.5f ,0.0f,     //derecha abajo    2
-         0.5f , 0.5f ,0.0f      //derecha arriba   3
+        //Position              //Color
+        -0.5f ,-0.5f ,0.0f,    1.0f,0.0f,0.0f,
+         0.5f ,-0.5f ,0.0f,    0.0f,1.0f,0.0f,
+         0.0f , 0.5f ,0.0f,    0.0f,0.0f,1.0f
     };
 
-    unsigned int indices[] =
-    {
-        0,1,2,      //triangulo 1
-        2,3,0       //triangulo 2
-    };
+
 
     //buffer para pasar vertex data
-    unsigned int VBO,VAO,EBO;
+    unsigned int VBO,VAO;
     glGenVertexArrays(1,&VAO);
     glGenBuffers(1,&VBO);
-    glGenBuffers(1,&EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(VertexData),VertexData,GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 *sizeof(float),(void *)0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6 *sizeof(float),(void *)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER,0);
-    glBindVertexArray(0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void *)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glUseProgram(shaderProgram);    
+
 
     //game loop 
     while(!glfwWindowShouldClose(window))
@@ -142,9 +140,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
         //revisar y cambiar
         glfwSwapBuffers(window);
@@ -154,7 +151,6 @@ int main()
 
     glDeleteVertexArrays(1,&VAO);
     glDeleteBuffers(1,&VBO);
-    glDeleteBuffers(1,&EBO);
     glfwTerminate();
 
 
